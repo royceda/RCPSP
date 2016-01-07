1fd70923db33ee36e552c2fa9049d7acc35cd6af
royceda <caucricity@gmail.com>
2016-01-07 18:37:37 +0100

    config interdites

---
+++ RCPSP_NetBeans_Project/ConfigInt.cpp
@@ -0,0 +1,69 @@
+#include <ilcplex/ilocplex.h>
+#include <iostream>
+
+#include "ConfigInt.hpp"
+
+ILOSTLBEGIN
+
+typedef IloArray< IloArray<IloNumArray> > NumMatrix;
+
+using namespace  std;
+
+
+ConfigInt::ConfigInt(){}
+
+ConfigInt::ConfigInt(Parser &p): _n(p.jobs()),_bigM(1000000), _T(p.getHorizon()), _r(p.nOfRes()), model(env),  S(env, p.jobs()), constraints(env), x(env, p.jobs()){
+
+ 
+  for (int i = 0; i < _n; i++) {
+    x[i] = IloArray<IloNumVar> (env, _n);
+    char name1[256];
+    sprintf(name1, "S %d ", i);
+    S[i] = IloNumVar(env, 0, p.getHorizon() - p.durationsVector()[i], ILOINT, name1);
+    //f[i] = IloArray <IloArray <IloNumVar> > (env, _n);
+    
+    for (int j = 0; j < _n; j++) {
+      char name[256];
+      sprintf(name, "X %d %d", i, j);
+      x[i][j] = IloNumVar(env, 0, 1, ILOBOOL, name);
+      //f[i][j] = IloArray<IloNumVar>(env, p.nOfRes());
+      for (int k = 0; k < p.nOfRes(); k++) {
+	sprintf(name, "f %d %d %d", i, j, k);
+	//f[i][j][k] = IloNumVar(env, 0, p.resAvail()[k], ILOINT, name);
+      }
+    }
+  }
+}
+
+
+IloObjective ConfigInt::objective(){
+  IloObjective obj(env, S[_n - 1], IloObjective::Minimize, "OBJ");
+  return obj;
+}
+
+
+void ConfigInt::addConstraints(Parser &p){
+
+  /*Les deux premiere*/
+  for(int i = 0; i < _n; i++){
+    for(int j =0; j < _n; j++){
+      constraints.add(x[i][j] == 1);
+      constraints.add(  x[i][j] + x[i][j] <= 1);
+
+      for(int k = 0; k <_n; k++){	
+	constraints.add( 1 >=  x[i][j] + x[j][k] - x[i][k] );       
+      }
+    }
+  }
+
+
+  for(int i = 0; i < _n; i++){
+    for(int j = 0; j < _n; j++){
+      constraints.add(S[j] - S[i] - (p.durationsVector()[i] + bigM())*x[i][j] >= -bigM());
+    }
+  }
+  
+
+
+}
+
---
+++ RCPSP_NetBeans_Project/ConfigInt.hpp
@@ -0,0 +1,46 @@
+/*
+ * File:   Time_indexed.hpp
+ * Author: Charlie Brown
+ *
+ * Created on 5 janvier 2016, 17:58
+ */
+
+#ifndef CONFIG_INT_HPP
+#define	CONFIG_INT_HPP
+
+#include "Parser.h"
+#include <ilcplex/ilocplex.h>
+
+class ConfigInt{
+public:
+  ConfigInt();
+  ConfigInt(Parser &p);
+  void solve(Parser& p);
+  
+  int bigM();
+  
+  
+protected:
+  IloObjective objective();
+  void addConstraints(Parser &p);
+  
+  
+  
+private:
+  IloEnv env;
+  IloModel model;
+  IloRangeArray constraints; 
+  IloArray <IloArray<IloNumVar> > x;
+  IloArray<IloNumVar> S;
+  
+  
+  int _bigM;
+  int _n; 
+  int _T;
+  int _r  ;
+  
+};
+
+
+
+#endif	/* CONFIG_INT_HPP */
